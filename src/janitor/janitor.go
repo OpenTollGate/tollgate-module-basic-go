2d6208c718ddb9d09e7bcb8aa32a6991314d03f5
Your Name <you@example.com>
2025-05-12 17:18:38 +0200

    ensure relay

--- src/janitor/janitor.go
+++ src/janitor/janitor.go
@@ -82,6 +82,10 @@ func (j *Janitor) listenForNIP94Events() {
 				for {
 					fmt.Printf("Connecting to relay: %s\n", relayURL)
 					relay, err := j.configManager.RelayPool.EnsureRelay(relayURL)
+					if err != nil || relay == nil {
+						log.Errorf("Failed to ensure relay %s: %v", relayURL, err)
+						continue
+					}
 					if err != nil {
 						time.Sleep(retryDelay)
 						retryDelay *= 2
